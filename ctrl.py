6de53bb500da121f9a89d4f9df248422404221d9
17101448 <the_1_week@naver.com>
2022-12-24 08:21:02 +0900

    ctrl파일 변경 btn1을 calculate 메서드에 연결함 (미구현

--- ctrl.py
+++ ctrl.py
@@ -2,7 +2,8 @@ class Control:
     def __init__(self, view):
         self.view = view
         self.connectSignals()
-        
+    def calculator(self):
+        pass        
     def connectSignals(self):
-        self.view.btn1.clicked.connect(self.view.activateMessage)
+        self.view.btn1.clicked.connect(self.view.calculate)
         self.view.btn2.clicked.connect(self.view.clearMessage)
