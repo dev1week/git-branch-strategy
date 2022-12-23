573fcd63c9395fe2616f589f119d7dd86ee66d18
dev1week <rondo2860@gmail.com>
2022-12-24 08:28:15 +0900

    ui.py 변경, btn1 le1 메서드 명 setDisplay로 변경

--- ui.py
+++ ui.py
@@ -14,9 +14,31 @@ class View(QWidget):
         self.te1 = QPlainTextEdit()
         self.te1.setReadOnly(True)
         
+        self.btn1=QPushButton('Calc',self)
+        self.btn2=QPushButton('Clear',self)
+        
+        self.le1=QLineEdit('0',self)
+        self.le1.setAlignment(QtCore.Qt.AlignRight)
+        self.le1.setFocus(True)
+        self.le1.selectAll()
+        
         self.btn1=QPushButton('Message',self)
         self.btn2=QPushButton('Clear',self)
         
+        self.le1=QLineEdit('0',self)
+        self.le1.setAlignment(QtCore.Qt.AlignRight)
+        
+        self.le2=QLineEdit('0',self)
+        self.le2.setAlignment(QtCore.Qt.AlignRight)
+        
+        self.cb = QComboBox(self)
+        self.cb.addItems(['+', '-', '*', '/'])
+
+        hbox_formular = QHBoxLayout()
+        hbox_formular.addWidget(self.le1)
+        hbox_formular.addWidget(self.cb)
+        hbox_formular.addWidget(self.le2)
+
         hbox = QHBoxLayout()
         hbox.addStretch(1)
         hbox.addWidget(self.btn1)
