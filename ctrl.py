ba0b0f100215ebccb7f3ac048a6ae4d9a101a014
dev1week <rondo2860@gmail.com>
2022-12-25 19:30:14 +0900

    나누기 기능 예외 처리

--- ctrl.py
+++ ctrl.py
@@ -23,10 +23,12 @@ class Control:
         return a-b
     def mul(self, a,b):
         return a*b 
-    def dev(self, a,b):
-        if(b==0):
-            return 0 
-        else: 
-            return a/b
+    def div(self, a,b):
+        try:
+            if(b==0):
+                raise Exception('Division Error')
+        except Exception as e: 
+            return e 
+        return a/b
     def pow(self, a,b):
         return pow(a,b)
