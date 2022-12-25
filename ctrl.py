fcbad1077579bba0d015211a520b36cf0c5cb1b6
17101448 <the_1_week@naver.com>
2022-12-25 17:52:14 +0900

    덧셈연산 화면에 연결, ctrl.py

--- ctrl.py
+++ ctrl.py
@@ -2,11 +2,18 @@ class Control:
     def __init__(self, view):
         self.view = view
         self.connectSignals()
-    def calculator(self):
-        pass        
+    def calculate(self):
+        num1 = float(self.view.le1.text())
+        num2 = float(self.view.le2.text())
+        operator = self.view.cb.currentText()
+
+        if operator == '+':
+            return f'{num1}+{num2}={self.sum(num1, num2)}'
+        else:
+            return "Calculate Error"
     def connectSignals(self):
-        self.view.btn1.clicked.connect(self.view.calculate)
-        self.view.btn2.clicked.connect(self.view.clearMessage)
+        self.view.btn1.clicked.connect(lambda:self.view.setDisplay(self.calculate()))
+        self.view.btn2.clicked.connect(lambda:self.view.clearMessage)
     def sum(self, a, b):
         try:
             return a+b 
